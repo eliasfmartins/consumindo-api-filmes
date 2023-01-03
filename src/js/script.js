01990b4307bb9797653f5fd1c0cfffa0c69cee63
Elias Fonseca Martins <eliasmartinsdev8@gmail.com>
2023-01-03 09:25:26 -0300

    feat
    att btn

--- src/js/script.js
+++ src/js/script.js
@@ -155,15 +155,19 @@ controls.forEach((control) => {
 
     if (isLeft) {
       currentItem -= 1;
+   
     } else {
       currentItem += 1;
+
     }
 
     if (currentItem >= maxItems) {
       currentItem = 0;
+    
     }
     if (currentItem < 0) {
       currentItem = maxItems - 1;
+     
     }
     items.forEach((item) => item.classList.remove("current-item"));
     // console.log(currentItem);
@@ -172,8 +176,10 @@ controls.forEach((control) => {
       //items[currentItem].scrollIntoView({
       //vai tentar deixar o intem da vez no centro correspondente ao curreitem
       inline: "center",
-      // behavior: "smooth", so funfa na tela fullscreen por alguma razão buga quando  em versão mobile
+      // behavior: "smooth", 
+      // so funfa na tela fullscreen por alguma razão buga quando  em versão mobile
     });
     items[currentItem].classList.add("current-item");
+   
   });
 });
